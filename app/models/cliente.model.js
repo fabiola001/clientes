46146f9267340be965eef99cb1c299c44ef7d75a
fabiola001 <wfaby01ap@gmail.com>
2025-08-06 14:09:19 -0600

    Update cliente.model.js

--- app/models/cliente.model.js
+++ app/models/cliente.model.js
@@ -1,8 +1,8 @@
 //Utilizamos module.export para exportar objetos para que puedan ser utilizados en otras clases
-module.exports = (DataTypes, DataTypes) => {
+module.exports = (sequelize, DataTypes) => {
     // usamos el DataTypes.defina para "definir" el nombre de la entity en la BD, en este caso "cliente"
     // Usamos type.DataTypes para definir el tipo de datos de cada atributo de la entidad 
-    const Cliente = DataTypes.define("cliente", {
+    const Cliente = sequelize.define("cliente", {
         id_cliente: {
             type: DataTypes.INTEGER,
             primaryKey: true,
